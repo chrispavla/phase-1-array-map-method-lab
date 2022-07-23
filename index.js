8d4e0f0109f97bbbb4eb8a908164269bb5d9776a
Kristina Voroteliak <kristina.hodges7@gmail.com>
2022-07-23 19:58:41 -0400

    commit tree 478e4f48190f94729c68bd489905b49c67c5b1f2

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
+// const tutorials = [
+//   'what does the this keyword mean?',
+//   'What is the Constructor OO pattern?',
+//   'implementing Blockchain Web API',
+//   'The Test Driven Development Workflow',
+//   'What is NaN and how Can we Check for it',
+//   'What is the difference between stopPropagation and preventDefault?',
+//   'Immutable State and Pure Functions',
+//   'what is the difference between == and ===?',
+//   'what is the difference between event capturing and bubbling?',
+//   'what is JSONP?'
+// ];
+
+// const titleCased = () => {
+
+//   return tutorials.map(item => {
+
+//     let words = item.split(" ")
+//     let capWords = words.map(it => {
+//       let firstLetter = it.slice(0, 1).toUpperCase()
+//       let theRest = it.slice(1)
+//       return `${firstLetter}${theRest}`;
+//     })
+//     return capWords.join(" ")
+//   })
+
+// }
+
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map((title) => {
+    let token = title.split(" ");
+    // console.log(token);
+    let capitalizedToken = token.map(
+      (t) => t.charAt(0).toUpperCase() + t.slice(1)
+    );
+
+    let response = capitalizedToken.join(" ");
+    return response;
+  });
 }
+
+console.log(titleCased());
